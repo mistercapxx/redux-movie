c1037a05424d0ad4012fdba288b32f53035a19ec
Vladimir <test@example.com>
2024-12-24 16:05:15 +0200

    Fix routing for GitHub Pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,9 @@
-// import React from "react";
-// import AddMovieForm from "./AddMovieForm";
-// import MovieList from "./MovieList";
-// import store from "../store/store";
-// import { Provider } from "react-redux";
-// import { FilterProvider } from "../context/FilterContext";
-// import FilterControls from "./FilterControls";
-
-// function App() {
-//   return (
-//     <Provider store={store}>
-//       {/* ///access to store for all components inside that use Selector and Dispatch */}
-//       <FilterProvider>  
-//         {/* /// - access to context of filter */}
-//         {/* ///all components know about the only one state - filter */}
-//         <div>
-      
-//         <br/>
-//         <h1>New Movie ? </h1>
-//         <AddMovieForm />
-//         <br/>
-//         <h1>Movie Hub</h1>
-//         <FilterControls/>
-//         <br/>
-//         <MovieList />
-//       </div>
-//       </FilterProvider>
-//     </Provider>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
-// import AddMovieForm from './AddMovieForm';
-// import MovieListPage from './MovieListPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider} from 'react-redux';
 import store from "../store/store";
 import { FilterProvider } from '../context/FilterContext';
 import HomePage from './HomePage';
-// import MovieItem from './MovieItem';
 import MovieDetailsPage from './MovieDetailsPage';
 import MovieListPage from './MovieListPage';
 
@@ -48,13 +12,12 @@ function App() {
   return (
     <Provider store={store}>
 <FilterProvider> 
-    <Router>
+    <Router basename='/redux-movie'>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
-        {/* <Route path="/add-movie" element={<AddMovieForm/>}/> */}
-        {/* <Route path="/movies" element={<MovieListPage/>}/> */}
+   
         <Route path='/movies' element={<MovieListPage/>}/>
-        {/*  */}
+  
       <Route path='/movie/:movieId' element={<MovieDetailsPage/>}/> 
       </Routes>
     </Router>
